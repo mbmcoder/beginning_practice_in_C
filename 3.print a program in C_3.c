8a313389e7f8ebf6b516323b0828a552347e04dc
mbmcoder <123202548+mbmcoder@users.noreply.github.com>
2023-01-24 16:10:02 +0000

    check

---
+++ 3.print a program in C_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>              
+int main()
+{
+ printf("Hello World!");
+ printf("\n");
+ return 0;
+}
+#include<stdio.h>              
+int main()
+{
+ printf("Hello World!");
+ printf("\n");
+ printf("\n");
+ return 0;
+}
